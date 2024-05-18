67ed83985a6db6acbc5ddc88684dc5f373809a63
Mohd Jahangeer Alam <mjalam564@gmail.com>
2024-05-18 11:26:25 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+// alert( 3 + 3) // we are using nodejs, not browser
+console.log(3 
+    + 5)   // code readability should be high
+    console.log("ALAM");
+
+    let name = "Alam"
+    let age = 32
+    let isLoggedIn = false
+    let state;
+     //number => 2 to power 53
+     //bigint
+     //string => ""
+     //boolean => true/false
+     //null => standalone value
+     // undefined => 
+    //symbol => unique
+
+     //object 
+
+     console.log(typeof age);
+     console.log(typeof name);
+     console.log(typeof null);
+     console.log(typeof undefined);
