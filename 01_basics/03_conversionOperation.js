5bb7739d46c68330218e78007b43f51eaea06fec
Mohd Jahangeer Alam <mjalam564@gmail.com>
2024-05-18 11:49:22 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = 55
+let gender = "Male"
+console.log(typeof score);
+console.log(typeof (gender));
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+// "55" => 55
+// "55abc" => NaN
+// true => 1; false => 0
+let isLoggedIn = 1
+// let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 =>false
+// "" => false
+//"Alam" =>true
+
+let someNumber = 55
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
