1216d93fe5f856e864264605f8deda7659de525a
Mohd Jahangeer Alam <mjalam564@gmail.com>
2024-05-19 11:39:34 +0000

    chnages

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,3 +21,34 @@ let someNumber = 55
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// ***************************************Operations*******************************************
+
+let value = 4
+let negValue = -value
+ // console.log(negValue );
+ // console.log(2+2);
+ // console.log(3-3);
+ // console.log(4*4 );
+ // console.log(5**5 );
+ // console.log(3/4);
+ // console.log(3%5);
+
+ let str1 = "Hi"
+ let str2 = "ALAM" 
+ let str3 = str1 + str2
+  console.log(str3);
+
+  console.log("1" + 2);
+  console.log(1 + "2");
+  console.log("1" + 2 + 2);
+  console.log(1 + 2 + "2");
+  console.log(4 + 5 *6 % 2);
+
+  console.log(true);
+  console.log(+true);
+  console.log("");
+  console.log(+"");
+ let gameCounter = 200
+ ++gameCounter;
+ console.log(gameCounter);
