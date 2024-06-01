40ebf98835417b40ca145bcee42c2d879ee8e562
Mohd Jahangeer Alam <mjalam564@gmail.com>
2024-06-01 07:01:38 +0000

    function 1

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -42,7 +42,34 @@ function loginUserMessage(username = "Alam"){
     return `${username} just logged in` 
 }
 
-console.log(loginUserMessage("Hamza"))
+// console.log(loginUserMessage("Hamza"))
 
+function calculateCartPrice(val1, val2, ...num1){           // ... is rest operator and spread operator.  here ... used as rest operator which collect 
+                                                  //       all the values and put all in array
+    return num1
+}
 
+// console.log(calculateCartPrice(200,400,600,800));
 
+const user = {
+
+    username: "Hamza",
+    price: 199
+}
+function handleObject(anyobject){
+    
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+
+handleObject( {
+    username: "Alam",
+    price:599
+})
+
+const myNewArray = [200,400,300,500,800]
+function returnSecondValue(getArray){
+    return getArray[3]
+}
+console.log(returnSecondValue(myNewArray));
